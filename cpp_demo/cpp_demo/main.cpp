b1e45bb55a6203315ef67ede00c4a0ce7a9b9347
Fred Yang <fredyang@techtrex.com>
2023-07-31 11:31:29 +0800

    init project

---
+++ cpp_demo/cpp_demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_demo
+//
+//  Created by Fred Yang on 2023/7/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
